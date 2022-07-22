b8672c27dc41b9b505bb801274f3991f39790509
Sabrina Salviano <sabrina.salviano.0934@gmail.com>
2022-07-22 12:30:06 -0300

    upando outra parte da refatoração

--- cadastro/scripts/sorteio.js
+++ cadastro/scripts/sorteio.js
@@ -1,52 +1,47 @@
 let operacao = 'A';
+let indice_selecionado = -1;
+const frmCadastro = document.getElementById('frmCadastro');
 var usuarios=JSON.parse(localStorage.getItem('usuarios'));
 let resultSorteio = [];
 
 function numpar(e) {
     if (e % 2 == 0) {
-
         return true;
 
     } else {
         return false;
     }
 }
-let indice_selecionado = -1;
-const frmCadastro = document.getElementById('frmCadastro');
 
 function sorteio() {
     usuarios = JSON.parse(localStorage.getItem('usuarios'))
     let max = usuarios.length
     let sbody = document.querySelector('.modal-body-resultado');
     let slinhas = '';
-    if (max <= 3) {
-        alert("É necessário adicionar pelo menos 4 pessoas")
-        return;
-    }
-    else if (max % 2 != 0) {
-        alert("O numero de candidatos deve ser par")
-        return;
-    }
-    let resultado = [];
-    let num = 0;
-    for (let i = 0; i < usuarios.length; i++) {
-        num = Math.floor((Math.random() * max) + 0);
-        if (resultado.includes(num)) {
-            i--;
+    let valido = validaUsuarios(max);
+    
+    if (valido) {
+        let resultado = [];
+        let num = 0;
+        for (let i = 0; i < usuarios.length; i++) {
+            num = Math.floor((Math.random() * max) + 0);
+            if (resultado.includes(num)) {
+                i--;
+            }
+            else {
+                resultado.push(num);
+    
+            }
         }
-        else {
-            resultado.push(num);
-
+        for (let i = 0; i < resultado.length; i++) {
+            if (numpar(i)) {
+                slinhas += `<p> ${resultSorteio[resultado[i]]} - ${resultSorteio[resultado[i + 1]]}`
+            }
         }
+        sbody.innerHTML += slinhas;
+        openModal('resultado-modal')
+        resultSorteio = [];
     }
-    for (let i = 0; i < resultado.length; i++) {
-        if (numpar(i)) {
-            slinhas += `<p> ${resultSorteio[resultado[i]]} - ${resultSorteio[resultado[i + 1]]}`
-        }
-    }
-    sbody.innerHTML += slinhas;
-    openModal('resultado-modal')
-    resultSorteio = [];
 }
 
 document.querySelector('#btnSortear').addEventListener('click', function (e) {
---
+++ cadastro/scripts/validaUsusarios.js
@@ -0,0 +1,13 @@
+function validaUsuarios(max) {
+    if (max <= 3) {
+        alert("É necessário adicionar pelo menos 4 pessoas") 
+        return false;
+    }
+    else if (max % 2 != 0) {
+        alert("O numero de candidatos deve ser par")
+        return false;
+    }
+    else {
+        return true;
+    }
+}
